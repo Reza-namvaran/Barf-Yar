aeefcbc6cd06f417f0689e3aaa71dd1ddf4a1afb
Zodiac <kasraalirezaie25@gmail.com>
2025-07-18 15:49:14 +0330

    Create main_menu Buttons

--- bot/keyboards/main_menu.py
+++ bot/keyboards/main_menu.py
@@ -1,4 +1,8 @@
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
 def get_main_menu():
-    pass
+    first_markup = InlineKeyboardMarkup(row_width=2)
+    info_button = InlineKeyboardButton(text="Info",callback_data="data1")
+    activity_button = InlineKeyboardButton(text="Activity",callback_data="data2")
+    first_markup.add(activity_button,info_button)
+    return first_markup
