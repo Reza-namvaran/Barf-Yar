7f5a4ca4dc4e0e8acd9c36745c1975d17d90a809
mary <maryam.nokohan@gmail.com>
2025-07-20 11:42:04 +0330

    (feat): added edit_post function to handle and update the title for edited posts

--- bot/handlers/channel_posts.py
+++ bot/handlers/channel_posts.py
@@ -1,4 +1,4 @@
-from bot.db.activity import insert_activity
+from bot.db.activity import insert_activity , update_activity_title 
 from bot.utils.helpers import title_spliter
 
 
@@ -12,3 +12,15 @@ def post_handler(bot):
             insert_activity(msg.message_id , msg_title)
         except Exception as e:
             print(f"[Error] Unexpected error happend : {e}")
+     
+    @bot.edited_channel_post_handler(
+        content_types=["text", "photo", "video", "audio", "voice", "document"]
+    )        
+    def edit_post(msg):
+        try:
+            new_title = title_spliter(msg.text) if msg.content_type == "text" else title_spliter(msg.caption or "")
+            update_activity_title(msg.message_id , new_title)
+            print(f"[Title] updated for {msg.message_id}")
+        except Exception as e:
+            print(f"[Error] Unexpected error happend : {e}")
+ 
