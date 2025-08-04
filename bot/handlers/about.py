265d5d271b570ed31884fbdba444afc699d53477
Reza-namvaran <reza.namvaran823@gmail.com>
2025-08-04 12:13:41 +0330

    (refactor): Change about message

--- bot/handlers/about.py
+++ bot/handlers/about.py
@@ -1,5 +1,16 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
+
+PRIVATE_CHANNEL_ID = os.getenv("PRIVATE_CHANNEL_ID")
+ABOUT_ID=os.getenv("ABOUT_ID")
 
 def handle_about(bot):
     @bot.message_handler(func=lambda message: message.text == "درباره برف نو")
     def about(message):
-        bot.send_message(message.chat.id, "توضیحات مربوط به انجمن")
+        bot.copy_message(
+                chat_id=message.chat.id,
+                from_chat_id=PRIVATE_CHANNEL_ID,
+                message_id=ABOUT_ID
+            )
