c65fa6818c14f8ce0ab9046080c6289b5df4ada1
Kasra <kasraalirezaie25@gmail.com>
2025-08-14 13:34:49 +0330

    feat : added delete and add activity handlers

--- panel/internal/handlers/activities_handler.go
+++ panel/internal/handlers/activities_handler.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
-	"net/http"
-
+	"encoding/json"
+	"github.com/Reza-namvaran/Barf-Yar/panel/internal/storage"
 	"github.com/Reza-namvaran/Barf-Yar/panel/internal/templates"
+	"net/http"
+	"strconv"
 )
 
 func (h *Handlers) GetAllActivities(w http.ResponseWriter, r *http.Request) {
@@ -36,4 +38,45 @@ func (h *Handlers) GetAllActivities(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
+}
+
+// add
+func (h *Handlers) AddActivityHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var activity storage.Activity
+
+	if err := json.NewDecoder(r.Body).Decode(&activity); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	if err := h.activityService.AddActivity(&activity); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "activity added successfully"})
+
+}
+
+// delete
+func (h *Handlers) DeleteActivityHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	idStr := r.URL.Path[len("/dashboard/activities/delete"):]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusInternalServerError)
+		return
+	}
+	if err := h.activityService.DeleteActivity(id); err != nil {
+		http.Error(w, "faild to delete activity", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "activity deleted successfully"})
 }
