9edec4d1ea320aa6d2e8ae69f103353d34f9d9dc
Reza-namvaran <reza.namvaran823@gmail.com>
2025-08-15 00:23:41 +0330

    (chore): Add simple activity seed

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO activities (message_id, title) VALUES (1, 'Activity 1');
+INSERT INTO activities (message_id, title) VALUES (2, 'Activity 2');
+INSERT INTO activities (message_id, title) VALUES (3, 'Activity 2');
+INSERT INTO activities (message_id, title) VALUES (4, 'Activity 2');
+INSERT INTO activities (message_id, title) VALUES (5, 'Activity 2');
